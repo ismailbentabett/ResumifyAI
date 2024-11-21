9d29e444d6a7a0fafc3cf1051a256545c9a58ee7
Ismail Bentabet <ismailbentabett@gmail.com>
2024-11-21 15:13:27 +0100

    chnanges

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
-import OgImage from '../public/banner.jpeg';
+import OgImage from '../public/banner.png';
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
 import { Analytics } from '@vercel/analytics/react';
